c71f7c3224f0c39ff141551d92bff05f505b16b7
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-01-02 19:40:47 -0500

    refresh

--- src/api/pokemonApi.js
+++ src/api/pokemonApi.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 
 const pokemonApi=axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/pokemon'
+    baseURL: 'https://pokeapi.co/api/v2/pokemon',
+    
 
 
 })
+
+
 export default pokemonApi
+//
